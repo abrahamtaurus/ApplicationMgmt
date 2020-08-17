c5af940f6a54a534abb7608bfde752431f02dbad
Hima Gopal <himag87621@gmail.com>
2020-08-17 16:02:48 +0530

    Add files via upload

---
+++ dbConnection.php
@@ -0,0 +1,28 @@
+
+<?php 
+
+class dbConnection
+{
+    private function __construct(){}
+    
+    public static function getConnection(){
+        $conn=null;
+        try 
+        {
+            $conn = new PDO("mysql:host=localhost; dbname=ApplicationMgmt", root, '');
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e) 
+        {
+            echo "Error: " . $e->getMessage();
+        }
+        return conn;
+    }
+    
+    public static function closeConnection($pconn)
+    {
+        $pconn->close();
+    }
+}
+?>
+
