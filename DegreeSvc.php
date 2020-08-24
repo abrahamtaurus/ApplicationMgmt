53800bd1e79a9acf881bbe16ce61e20e5bba41b1
anushadsouza19 <69142038+anushadsouza19@users.noreply.github.com>
2020-08-24 17:24:07 +0530

    Update DegreeSvc.php

--- DegreeSvc.php
+++ DegreeSvc.php
@@ -4,45 +4,33 @@
 *Purpose : 
 *Date : 24/08/2020
 **/
-include_once 'Degree.php';
-include_once  "../core/dBConnection.php";
-class DegreeRepo
+include_once "Degree.php";
+include_once "DegreeRepo.php";
+class DegreeSvc
 {
-    private $dbCon;
-    public function __construct()
+	private $degreeRepo;
+	public function __construct()
     {
-        $this->dbCon = dbConnection::getConnection();
-    }
-    public function insert($pobjDegree){
+		$this->degreeRepo = new DegreeRepo();
+	}
+	public function save($pobjDegree){
 		$vobjDegree = $pobjDegree;
-		$strSQLStmt = "INSERT INTO tbl_degree(name, is_deleted) VALUES(?,?)";
-		$stmt = $this->dbCon->prepare( $strSQLStmt);
-        $stmt->execute([$pobjDegree->getname(), 0]);
-        $vobjDegree->setId($this->dbCon->lastInsertId());
-        //$stmt->close();
+        if($pobjDegree->getId()==-1)
+        {
+			$vobjDegree = $this->degreeRepo->insert($pobjDegree);
+		}
+		else
+		{
+			$vobjDegree = $this->degreeRepo->update($pobjDegree);
+		}	  
 		return $vobjDegree;
-    }
-    
-    public function update($pobjDegree){
-        $strSQLStmt="UPDATE tbl_degree SET name=? WHERE id=?";
-        $stmt = $this->dbCon->prepare($strSQLStmt);
-        $stmt->execute([$pobjDegree->getname(),$pobjDegree->getId()]);
-        //$stmt->close();
-        return $pobjDegree;
-    }
+	  }
+	  
+	  
+	  public function delete($pobjDegree){
+		$this->degreeRepo->delete($pobjDegree);
+	  }
+	  
+}
 
-    public function delete($pobjDegree){
-        $strSQLStart="UPDATE tbl_degree SET is_deleted=1 WHERE id=?"; 
-        $stmt = $this->dbCon->prepare($strSQLStmt);
-        $stmt->execute([$pobjDegree->getId()]);
-       // $stmt->close();
-
-        return $pobjDegree;
-    }
-
-    function __destruct() 
-    {
-        //dbConnection::closeConnection($this->dbCon);
-    }  
-} 
-?>
+?> 
