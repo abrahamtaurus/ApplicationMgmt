2dc25900b5a765bf9cb061a5b8a9c031279b94c7
Hima Gopal <himag87621@gmail.com>
2020-08-24 17:00:59 +0530

    Update Country.php

--- Country.php
+++ Country.php
@@ -1,36 +1,50 @@
+/**
+* Author:Hima Gopal
+* Purpose:
+* Date:23/08/2020
+*/
 <?php
-include 'dbConnection.php';
 class Country {
   private $id;
-  private $country_name;
+  private $name;
   private $host;
 
-  public function __construct($pid, $pcountryName, $phost)
+  public function __construct($pid, $pname, $phost)
   {
 	$this->id = $pid;
-	$this->country_name = $pcountryName;
+	$this->name = $pname;
 	$this->host = $phost;
   }
   
-  public function setid($pid)
+  public function setId($pid)
   {
 	$this->id = $pid;
   }
-  
- public function setcountry_name($pcountryName)
-  {
-	$this->country_name = $pcountryName;
+
+  public function getId(){
+    return $this->id;
   }
   
+ public function setname($pname)  {
+    $this->name = $pname;
+  }
   
-  public function sethost($phost)
-  {
-	$this->host = $phost;
+  public function getname()  {
+    return $this->name;
+  }
+  
+ 
+  public function sethost($phost){
+	   $this->name =$phost;
   }
  
+  public function gethost(){
+	  return $this->host;
+  }
+
   public function toJSON()
   {
-        return json_encode("{id: $this->id, country_name:$this->country_name, host: $this->host}");
+        return json_encode("{id: $this->id, name:$this->name, host: $this->host}");
   }
 }
 ?>
