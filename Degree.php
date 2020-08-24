51919300c03faaa6093369f4c1e36575f8048272
anushadsouza19 <69142038+anushadsouza19@users.noreply.github.com>
2020-08-24 17:01:13 +0530

    Update and rename degree.php to Degree.php

--- Degree.php
+++ Degree.php
@@ -1,7 +1,10 @@
 <?php
-//degree
-include 'dbConnection.php';
-class degree {
+/**
+*Author : Anusha DSouza
+*Purpose : 
+*Date : 24/08/2020
+**/
+class Degree {
   private $id;
   private $name;
  
@@ -10,7 +13,6 @@ class degree {
   {
 	$this->id = $pid;
 	$this->name = $pname;
-	
   }
   
   public function setId($pid)
@@ -18,11 +20,20 @@ class degree {
 	$this->id = $pid;
   }
   
+  public function getId()
+  {
+    return $this->id;
+  }
+  
  public function setname($pname)
   {
 	$this->name = $pname;
   }
   
+  public function getname()
+  {
+    return $this->name;
+  }
   
   
   public function toJSON()
