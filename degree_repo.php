ed000c186101f4cf1aab182a668e8d54ebe0e7f1
anushadsouza19 <69142038+anushadsouza19@users.noreply.github.com>
2020-08-24 17:25:25 +0530

    Update degree_repo.php

--- degree_repo.php
+++ degree_repo.php
@@ -1,27 +1,48 @@
 <?php
-//degree_repo
-include 'degree.php';
-#include_once "dbConnection.php";
-class degree_repo
+/**
+*Author : Anusha DSouza
+*Purpose : 
+*Date : 24/08/2020
+**/
+include_once 'Degree.php';
+include_once  "../core/dBConnection.php";
+class DegreeRepo
 {
     private $dbCon;
     public function __construct()
     {
         $this->dbCon = dbConnection::getConnection();
     }
-    public function insert($pobjdegree) 
-    {
-        $strSQLStmt = "INSERT INTO degree(id,name) VALUES (?, ?)";
-        
-        $stmt = $conn->prepare($strSQLStart);
-        $stmt->bindParam($pobjdegree->getid(),$pobjdegree->getname());
-        
-        $stmt->execute();
-        $stmt->close();
+    public function insert($pobjDegree){
+		$vobjDegree = $pobjDegree;
+		$strSQLStmt = "INSERT INTO tbl_degree(name, is_deleted) VALUES(?,?)";
+		$stmt = $this->dbCon->prepare( $strSQLStmt);
+        $stmt->execute([$pobjDegree->getname(), 0]);
+        $vobjDegree->setId($this->dbCon->lastInsertId());
+        //$stmt->close();
+		return $vobjDegree;
     }
+    
+    public function update($pobjDegree){
+        $strSQLStmt="UPDATE tbl_degree SET name=? WHERE id=?";
+        $stmt = $this->dbCon->prepare($strSQLStmt);
+        $stmt->execute([$pobjDegree->getname(),$pobjDegree->getId()]);
+        //$stmt->close();
+        return $pobjDegree;
+    }
+
+    public function delete($pobjDegree){
+        $strSQLStart="UPDATE tbl_degree SET is_deleted=1 WHERE id=?"; 
+        $stmt = $this->dbCon->prepare($strSQLStmt);
+        $stmt->execute([$pobjDegree->getId()]);
+       // $stmt->close();
+
+        return $pobjDegree;
+    }
+
     function __destruct() 
     {
-        dbConnection::closeConnection($this->dbCon);
+        //dbConnection::closeConnection($this->dbCon);
     }  
 } 
 ?>
