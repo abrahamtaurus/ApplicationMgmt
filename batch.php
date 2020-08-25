82e33b0be8f33336e7de07e60836646d205a606c
sushumnaspradeep <57635131+sushumnaspradeep@users.noreply.github.com>
2020-08-25 11:45:38 +0530

    Update batch.php

--- batch.php
+++ batch.php
@@ -1,42 +1,49 @@
-
+/**
+*Author:Sushumna S Pradeep
+*Purpose:
+*Date:23/08/2020  
+*/
 <?php
-include "dbconnection.php";
-Class batch
-{
-    private $id; 
-    private $year_2;
-    private $is_open;
+class Batch {
+  private $id;
+  private $year;
+  private $isOpen;
 
-    
-    function __construct($pid,$pcourse_id, $pyear_2, $pis_open)
-    {
-        $this->setid($pid);
-        $this->setcourse_id($pcourse_id);
-        $this->setyear_2($pyear_2);
-        $this->setis_open($pis_open);
-    }
-    public function setid($pid)
-    {
-        $this->id = $pid;
-    } 
-    public function setcourse_id($pcourse_id)
-    {
-        $this->course_id = $pcourse_id;
-    } 
-    
-    public function setyear_2($pyear_2)
-    {
-        $this->year_2 = $pyear_2;
-        
-    }
-    public function setis_open($pis_open)
-    {
-        $this->is_open = $pis_open;
-        
-    }
-    public function toJSON()
-    {
-        return json_encode("{id: $this->id,course_id:$this->course_id year_2:$this->year_2, is_open: $this->is_open");
-    }
+  public function __construct($pid,  $pyear, $pisOpen)
+  {
+	$this->id = $pid;
+	$this->year = $pyear;
+	$this->isOpen = $pisOpen;
+  }
+  
+  public function setId($pid)
+  {
+	$this->id = $pid;
+  }
+
+  public function getId(){
+    return $this->id;
+  }
+ 
+  public function setyear($pyear){
+	  return $this->year =$pyear;
+  }
+ 
+  public function getyear(){
+	  return $this->year;
+  }
+
+  public function setisOpen($pisOpen){
+	$this->isOpen = $pisOpen;
+  }
+  
+  public function getisOpen(){
+	  return $this->isOpen;
+  }
+
+  public function toJSON()
+  {
+        return json_encode("{id: $this->id, year: $this->year, isOpen: $this->isOpen}");
+  }
 }
 ?>
