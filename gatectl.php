fec809310897a8bfd47ee6f5bc2c1fa894ba6dc2
sushumnaspradeep <57635131+sushumnaspradeep@users.noreply.github.com>
2020-08-27 16:08:05 +0530

    Update gatectl.php

--- gatectl.php
+++ gatectl.php
@@ -1,26 +1,33 @@
-<?php
-include_once "gate.php";
-#include_once "gatesvc.php";
 
-class gatectl
+/**
+*Author:Sushumna S Pradeep
+*Purpose:
+*Date:23/08/2020  
+*/
+<?php
+include_once "Gate.php";
+include_once "GateSvc.php";
+
+class GateCtl
 {
     private $vobjApplSvc;
+    private $applData;
     function __construct()
     {
-        //$this->vobjApplSvc = new CandidateSvc();        
+        $this->vobjApplSvc = new GateSvc();        
     }
     public function doPost()
     {
        // $vjsonCandidate = json_decode($_POST['applData']);
         $vjsongate = json_decode(file_get_contents('php://input'));
-        var_dump($vjsongate);
-        $vobjgate = new gate($vjsongate->id, $vjsongate->examDate, $vjsongate->discipline,$vjsongate->score,$vjsongate->percentile,$vjsongate->air,$vjsongate->validity);
-        
+        //var_dump($vjsongate);
+        $vobjgate = new Gate($vjsongate->id,$vjsongate->examDate, $vjsongate->discipline,$vjsongate->score,$vjsongate->percentile,$vjsongate->air,$vjsongate->validity);
+        $vobjgate = $this->vobjApplSvc->save($vobjgate);
         echo $vobjgate->toJSON();
     
     }    
     //$this->vobjApplSvc->save($vjsonCandidate);  
 }
-$appl = new gatectl();
+$appl = new GateCtl();
 $appl->doPost();                                                    
 ?>
