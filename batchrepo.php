2c36ac141996a1f32d96434817241fed6c827acf
sushumnaspradeep <57635131+sushumnaspradeep@users.noreply.github.com>
2020-08-27 16:01:00 +0530

    Update batchrepo.php

--- batchrepo.php
+++ batchrepo.php
@@ -1,26 +1,49 @@
+/**
+*Author:Sushumna S Pradeep
+*Purpose:
+*Date:23/08/2020  
+*/
 <?php
-include "batch.php";
-
-Class batchrepo
+include_once 'Batch.php';
+include_once  "../core/dBConnection.php";
+class BatchRepo
 {
-    private $dbConn;
-        
-    function __construct()
+    private $dbCon;
+    public function __construct()
     {
-        $this->dbConn = DBConnection::getConnection();
+        $this->dbCon = dbConnection::getConnection();
     }
-    public function insert($pobjbatch)
-    {
-        $SQLStmt = "INSERT INTO batch(course_id,year_2, is_open ) VALUES (?,?, ?)";
-        $stmt = $conn->prepare($strSQLStmt);
-        $stmt->bindParam($pobjbatch->getcourse_id(),$pobjbatch->getyear_2(),$pobjbatch->getis_open());
-        $stmt->execute();
-        $stmt->close();
+    public function insert($pobjBatch){
+		$vobjBatch = $pobjBatch;
+		$strSQLStmt = "INSERT INTO tbl_batch(course_id, year_2, is_open,is_deleted) VALUES(?,?,?,?)";
+		$stmt = $this->dbCon->prepare( $strSQLStmt);
+        $stmt->execute([1, $pobjBatch->getyear(),$pobjBatch->getisOpen(),0]);
+        $vobjBatch->setId($this->dbCon->lastInsertId());
+        //$stmt->close();
+		return $vobjBatch;
     }
-    function __destruct()
-    {
-        DbConnection::closeConnection($this->dbCon);
-    }   
-}
     
+    public function update($pobjBatch){
+        $strSQLStmt="UPDATE tbl_batch SET year_2=?, is_open=? WHERE id=?";
+        $stmt = $this->dbCon->prepare($strSQLStmt);
+        $stmt->execute([$pobjBatch->getyear(),$pobjBatch->getisOpen(), $pobjBatch->getId()]);
+        //$stmt->close();
+        return $pobjBatch;
+    }
+
+    public function delete($pobjBatch){
+        $strSQLStart="UPDATE tbl_batch SET is_deleted=1 WHERE id=?"; 
+        $stmt = $this->dbCon->prepare($strSQLStmt);
+        $stmt->execute([$pobjBatch->getId()]);
+       // $stmt->close();
+
+        return $pobjBatch;
+    }
+
+    function __destruct() 
+    {
+        //dbConnection::closeConnection($this->dbCon);
+    }  
+} 
 ?>
+
