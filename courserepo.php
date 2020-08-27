190b02c127b0bd7189384eb0fcd4b46b635fdda9
sushumnaspradeep <57635131+sushumnaspradeep@users.noreply.github.com>
2020-08-27 16:04:24 +0530

    Update courserepo.php

--- courserepo.php
+++ courserepo.php
@@ -1,28 +1,53 @@
-<?php
-include 'course.php';
 
-class courserepo
+/**
+*Author:Sushumna S Pradeep
+*Purpose:
+*Date:23/08/2020  
+*/
+<?php
+include_once 'Course.php';
+include_once  "../core/dBConnection.php";
+class CourseRepo
 {
     private $dbCon;
-	public function __construct()
-	{
-        $this->dbCon = DbConnection::getConnection();
+    public function __construct()
+    {
+        $this->dbCon = dbConnection::getConnection();
     }
-	public function insert($pobjcourse) 
-	{
-        $strSQLStmt = ("INSERT INTO course(course_name, is_active ) VALUES (? ,?)");
-        
-        $stmt = $conn->prepare($strSQLStmt);
-        $stmt->bindParam($pobjcourse->getcourse_name(),$pobjcourse->getis_active());
-        
-        $stmt->execute();
-        $stmt->close();
+    public function insert($pobjCourse){
+		$vobjCourse = $pobjCourse;
+		$strSQLStmt = "INSERT INTO tbl_course (course_name, is_active,is_deleted) VALUES(?,?,?)";
+		$stmt = $this->dbCon->prepare( $strSQLStmt);
+        $stmt->execute([ $pobjCourse->getcourseName(),$pobjCourse->getisActive(),0]);
+        $vobjCourse->setId($this->dbCon->lastInsertId());
+        //$stmt->close();
+		return $vobjCourse;
     }
     
-	function __destruct()
-	{
-        DbConnection::closeConnection($this->dbCon);
-    }   
-}
+    public function update($pobjCourse){
+        $strSQLStmt="UPDATE tbl_course SET course_name=?, is_active=? WHERE id=?";
+        $stmt = $this->dbCon->prepare($strSQLStmt);
+        $stmt->execute([$pobjCourse->getcourseName(),$pobjCourse->getisActive(), $pobjCourse->getId()]);
+        //$stmt->close();
+        return $pobjCourse;
+    }
 
+    public function delete($pobjCourse){
+        $strSQLStart="UPDATE tbl_course SET is_deleted=1 WHERE id=?"; 
+        $stmt = $this->dbCon->prepare($strSQLStmt);
+        $stmt->execute([$pobjCourse->getId()]);
+       // $stmt->close();
+
+        return $pobjCourse;
+    }
+
+    function __destruct() 
+    {
+        //dbConnection::closeConnection($this->dbCon);
+    }  
+} 
 ?>
+
+
+
+
